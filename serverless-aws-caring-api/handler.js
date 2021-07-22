c18593cb7a4ff0bdb0d0530824df895f392f5f10
Shubham Kumar <47415702+ShenpaiSharma@users.noreply.github.com>
2021-07-22 12:12:08 +0530

    Updated handler.js

--- serverless-aws-caring-api/handler.js
+++ serverless-aws-caring-api/handler.js
@@ -14,28 +14,3 @@ const server = awsServerlessExpress.createServer(app);
 exports.handler = (event, context) => {
   return awsServerlessExpress.proxy(server, event, context);
 }
-
-
-// const serverless = require("serverless-http");
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res, next) => {
-//   return res.status(200).json({
-//     message: "Hello from root!",
-//   });
-// });
-
-// app.get("/hello", (req, res, next) => {
-//   return res.status(200).json({
-//     message: "Hello from path!",
-//   });
-// });
-
-// app.use((req, res, next) => {
-//   return res.status(404).json({
-//     error: "Not Found",
-//   });
-// });
-
-// module.exports.handler = serverless(app);
